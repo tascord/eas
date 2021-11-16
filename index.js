7ce9ccb047a74cdeb3c08e53f757986c115fb518
tascord <theactivesquare@gmail.com>
2021-11-16 20:43:24 +1100

    Content warning but i only did it so i could get a page interaction

--- index.js
+++ index.js
@@ -1,217 +1,224 @@
-const canvas = document.querySelector('canvas');
-const context = canvas.getContext('2d');
+function EAS() {
+    const canvas = document.querySelector('canvas');
+    const context = canvas.getContext('2d');
 
-const Script = Config.script.split('\n').map(l => l.trimStart()).join('\n');
-const Speech = window.speechSynthesis || { speaking: false };
+    const Script = Config.script.split('\n').map(l => l.trimStart()).join('\n');
+    const Speech = window.speechSynthesis || { speaking: false };
 
-if(window.speechSynthesis) {
-    Speech.cancel();
-}
-
-const eas = document.querySelector('#eas');
-eas.volume = Config.eas_volume ?? 0.05;
-eas.play();
-
-const static = document.querySelector('#static')
-static.volume = Config.static_volume ?? 0.05;
-static.loop = true;
-static.play();
-
-// Draw
-requestAnimationFrame(draw);
-
-// Default scaling
-scale();
-
-// Handle scaling
-document.body.onresize = scale;
-
-function scale() {
-    const { width, height } = canvas.getBoundingClientRect();
-    canvas.width = width * 0.4;
-    canvas.height = height * 0.4;
-}
-
-function draw() {
-
-    setTimeout(() => requestAnimationFrame(draw), 70);
-
-    const w = canvas.width;
-    const h = canvas.height;
-
-    // Clear background
-    context.fillStyle = 'black';
-    context.fillRect(0, 0, w, h);
-
-    // Draw borders
-    context.strokeStyle = 'white';
-    context.lineWidth = '3';
-    const line_scales = [
-        [11.25, 7.5],
-        [6.5, 5],
-        [4.75, 5],
-    ];
-
-    for (let [x, y] of line_scales) {
-        context.strokeRect(w / x, h / y, w - ((w / x) * 2), h - ((h / y) * 2));
+    if (window.speechSynthesis) {
+        Speech.cancel();
     }
 
-    // Draw text
-    draw_text()
+    const eas = document.querySelector('#eas');
+    eas.volume = Config.eas_volume ?? 0.05;
+    eas.play();
 
-    document.querySelector('img').src = corrupt(canvas.toDataURL('image/jpeg'));
+    const static = document.querySelector('#static')
+    static.volume = Config.static_volume ?? 0.05;
+    static.loop = true;
+    static.play();
 
-}
+    // Draw
+    requestAnimationFrame(draw);
 
-let lines = [0, 0];
+    // Default scaling
+    scale();
 
-function draw_text() {
+    // Handle scaling
+    document.body.onresize = scale;
 
-    const w = canvas.width;
-    const h = canvas.height;
-
-    context.textAlign = 'center';
-    context.textBaseline = 'middle';
-    context.fillStyle = 'white';
-
-    const [start, end] = lines;
-    let slices = Script
-        .split('\n')
-        .slice(start, end)
-        .filter(l => l.trim() !== '' || l.trim() !== '-');
-
-    const line_spacing = (h / 20);
-    const font = 'VCR';
-
-    if(!eas.paused) {
-        slices = [
-            `#EMERGENCY ALERT SYSTEM`,
-            `,${new Date().toGMTString()}`,
-            `EAS Participant issued a`,
-            `██████ ████████ EMERGENCY`
-        ]
+    function scale() {
+        const { width, height } = canvas.getBoundingClientRect();
+        canvas.width = width * 0.4;
+        canvas.height = height * 0.4;
     }
 
-    for (let index = 0; index < slices.length; index++) {
+    function draw() {
 
-        let line = slices[index];
-        let y_pos = (h / 2) - (slices.length * line_spacing / 2) + index * line_spacing;
+        setTimeout(() => requestAnimationFrame(draw), 70);
 
-        // Header
-        if (line.startsWith('#')) {
-            line = line.slice(1).trim();
-            context.font = `20px ${font}`;
-            context.fillText(line, w / 2, y_pos);
+        const w = canvas.width;
+        const h = canvas.height;
+
+        // Clear background
+        context.fillStyle = 'black';
+        context.fillRect(0, 0, w, h);
+
+        // Draw borders
+        context.strokeStyle = 'white';
+        context.lineWidth = '3';
+        const line_scales = [
+            [11.25, 7.5],
+            [6.5, 5],
+            [4.75, 5],
+        ];
+
+        for (let [x, y] of line_scales) {
+            context.strokeRect(w / x, h / y, w - ((w / x) * 2), h - ((h / y) * 2));
         }
 
-        // Sub-Header
-        else if (line.startsWith(',')) {
-            line = line.slice(1).trim();
-            context.font = `13px ${font}`;
-            context.fillText(line, w / 2, y_pos);
+        // Draw text
+        draw_text()
+
+        document.querySelector('img').src = corrupt(canvas.toDataURL('image/jpeg'));
+
+    }
+
+    let lines = [0, 0];
+
+    function draw_text() {
+
+        const w = canvas.width;
+        const h = canvas.height;
+
+        context.textAlign = 'center';
+        context.textBaseline = 'middle';
+        context.fillStyle = 'white';
+
+        const [start, end] = lines;
+        let slices = Script
+            .split('\n')
+            .slice(start, end)
+            .filter(l => l.trim() !== '' || l.trim() !== '-');
+
+        const line_spacing = (h / 20);
+        const font = 'VCR';
+
+        if (!eas.paused) {
+            slices = [
+                `#EMERGENCY ALERT SYSTEM`,
+                `,${new Date().toGMTString()}`,
+                `EAS Participant issued a`,
+                `██████ ████████ EMERGENCY`
+            ]
         }
 
-        // Regular text
-        else {
+        for (let index = 0; index < slices.length; index++) {
 
-            context.font = `12px ${font}`;
-            line = line.replace(/<.+?>/g, (data) => {
+            let line = slices[index];
+            let y_pos = (h / 2) - (slices.length * line_spacing / 2) + index * line_spacing;
 
-                let [prop, val] = data.slice(1, -1).split('=');
+            // Header
+            if (line.startsWith('#')) {
+                line = line.slice(1).trim();
+                context.font = `20px ${font}`;
+                context.fillText(line, w / 2, y_pos);
+            }
 
-                switch (prop) {
+            // Sub-Header
+            else if (line.startsWith(',')) {
+                line = line.slice(1).trim();
+                context.font = `13px ${font}`;
+                context.fillText(line, w / 2, y_pos);
+            }
 
-                    case 'size':
-                        context.font = `${val} ${font}`;
-                        break;
+            // Regular text
+            else {
 
-                    case 'colour':
-                    case 'color':
-                        context.fillStyle = val;
-                        break;
+                context.font = `12px ${font}`;
+                line = line.replace(/<.+?>/g, (data) => {
 
-                    default:
-                        return data;
-                }
+                    let [prop, val] = data.slice(1, -1).split('=');
+
+                    switch (prop) {
+
+                        case 'size':
+                            context.font = `${val} ${font}`;
+                            break;
+
+                        case 'colour':
+                        case 'color':
+                            context.fillStyle = val;
+                            break;
+
+                        default:
+                            return data;
+                    }
 
 
-                return '';
-            })
+                    return '';
+                })
 
-            context.fillText(line, w / 2, y_pos);
+                context.fillText(line, w / 2, y_pos);
+
+            }
 
         }
 
     }
 
-}
+    setInterval(update_text, (Config.speed ?? 3) * 1000);
+    update_text();
 
-setInterval(update_text, (Config.speed ?? 3) * 1000);
-update_text();
+    function update_text() {
 
-function update_text() {
+        if (Speech.speaking || !eas.paused) return;
+        let slices = Script.split('\n');
 
-    if (Speech.speaking || !eas.paused) return;
-    let slices = Script.split('\n');
-
-    if (slices[lines[1] + 1] !== undefined) {
-        lines[0] = lines[1];
-    } else {
-        if (Config.loop === true) {
-            eas.play();
-            lines = [0, 0];
+        if (slices[lines[1] + 1] !== undefined) {
+            lines[0] = lines[1];
+        } else {
+            if (Config.loop === true) {
+                eas.play();
+                lines = [0, 0];
+            }
+            return;
         }
-        return;
+
+        let end = lines[1];
+        if (slices[lines[1]].startsWith('-')) {
+            lines[0] = lines[1] + 1;
+            end++;
+        }
+
+        while (slices[end] !== undefined && !slices[end].startsWith('-') && end - lines[1] <= 6) {
+            end++;
+        }
+
+        lines[1] = end;
+
+        if (Config.speak) speak(slices.slice(lines[0], lines[1]).join(' '));
     }
 
-    let end = lines[1];
-    if (slices[lines[1]].startsWith('-')) {
-        lines[0] = lines[1] + 1;
-        end++;
+    function corrupt(data) {
+
+        let editable = data.split(',').pop().split('');
+
+        const corruption_value = Config.corruption || 10;
+        const replacements = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+
+        for (let _ = 0; _ < corruption_value + (5 - Math.floor(Math.random() * 10)); _++) {
+            let index = Math.floor(Math.random() * editable.length);
+            if (index < 30) continue;
+
+            editable[index] = replacements[Math.floor(Math.random() * replacements.length)]
+        }
+
+        return (data.split(',').shift() + ',' + editable.join(''));
+
     }
 
-    while (slices[end] !== undefined && !slices[end].startsWith('-') && end - lines[1] <= 6) {
-        end++;
+    function speak(line) {
+
+        console.log(`Speaking: ${line}`);
+        line = line.replace(/<.+?>/g, '').replace(/[^0-9a-z,.!? ]/gi, '')
+
+        if (!window.speechSynthesis) return;
+        let utterance = new SpeechSynthesisUtterance(line);
+
+        let voices = Speech.getVoices();
+        utterance.voice = voices.find(v => v.name.includes(Config.voice || 'David')) || voices[0];
+
+        utterance.pitch = Config.pitch || 0.1;
+        utterance.rate = Config.rate || 0.85;
+        utterance.volume = Config.voice_volume || 0.25;
+
+        Speech.speak(utterance);
+
     }
-
-    lines[1] = end;
-
-    if (Config.speak) speak(slices.slice(lines[0], lines[1]).join(' '));
 }
 
-function corrupt(data) {
-
-    let editable = data.split(',').pop().split('');
-
-    const corruption_value = Config.corruption || 10;
-    const replacements = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-
-    for (let _ = 0; _ < corruption_value + (5 - Math.floor(Math.random() * 10)); _++) {
-        let index = Math.floor(Math.random() * editable.length);
-        if (index < 30) continue;
-
-        editable[index] = replacements[Math.floor(Math.random() * replacements.length)]
-    }
-
-    return (data.split(',').shift() + ',' + editable.join(''));
-
-}
-
-function speak(line) {
-
-    console.log(`Speaking: ${line}`);
-    line = line.replace(/<.+?>/g, '').replace(/[^0-9a-z,.!? ]/gi, '')
-
-    if (!window.speechSynthesis) return;
-    let utterance = new SpeechSynthesisUtterance(line);
-
-    let voices = Speech.getVoices();
-    utterance.voice = voices.find(v => v.name.includes(Config.voice || 'David')) || voices[0];
-
-    utterance.pitch = Config.pitch || 0.1;
-    utterance.rate = Config.rate || 0.85;
-    utterance.volume = Config.voice_volume || 0.25;
-
-    Speech.speak(utterance);
-
+function ready() {
+    document.querySelector('.accept').remove();
+    EAS();
 }
